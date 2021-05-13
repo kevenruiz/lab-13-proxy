72656dffd8e21c2a520dd250602a564ff75476e7
keven-ruiz <kevenruizh@gmail.com>
2021-05-12 23:39:09 -0700

    Got help with adding location, lat and lng didnt w

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,14 @@
 import app from '../lib/app.js';
 import supertest from 'supertest';
 
+// I did not get that far on writting the test. will finish this up this weekend. 
+
 const request = supertest(app);
 
 describe('API Routes', () => {
-  it('someday ill put a better message', () => {
+  it('test location function', () => {
+    const expectation = {
+
+    }
   });
 });
