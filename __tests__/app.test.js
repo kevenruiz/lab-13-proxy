8061763c2c6f9ca31cc89ff81922f15c0222aca4
keven-ruiz <kevenruizh@gmail.com>
2021-05-18 16:14:13 -0700

    making changes before getting TA help.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,2 +1,31 @@
 import formatLocation from '../lib/munge-utils';
-import LocationData from
+import LocationData from '../data/location-data';
+
+describe('API Data Munging', () => {
+
+  const expectedLocation =
+
+  {
+    'formatted_query': 'Avenida Calle 30, Barranquillita, Barranquilla, Atlántico',
+    'latitude': '10.9777174',
+    'longitude': '-74.7765211'
+  }
+    ;
+
+
+
+  it('munges movie data', async () => {
+    // arrange
+    // expected is in variable above
+    // movieData is imported from file
+
+    // act 
+    const output = formatLocation(LocationData);
+
+    // assert
+    expect(output[0]).toEqual(expectedLocation);
+  });
+
+
+
+});
